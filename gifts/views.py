748ebb7de80197a251e048388c6783e12d7aa0d3
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-08-14 23:26:25 +0400

    Stage 15: add multiplayer gift send endpoint

--- gifts/views.py
+++ gifts/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from restaurants.models import Restaurant
 
 from .models import Gift, InventoryGift
-from .serializers import GiftSerializer, InventoryGiftSerializer
+from .serializers import (
+    GiftSerializer,
+    InventoryGiftSerializer,
+    SendGiftSerializer,
+)
+from .services import send_gift_to_room_players
 
 
 class RestaurantGiftListView(ListAPIView):
@@ -44,3 +51,21 @@ class InventoryGiftDetailView(RetrieveAPIView):
             InventoryGift.objects.filter(owner=self.request.user)
             .select_related("gift", "gift__restaurant")
         )
+
+
+class SendRoomGiftView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, room_id):
+        serializer = SendGiftSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        result = send_gift_to_room_players(
+            sender_user=request.user,
+            room_id=room_id,
+            gift_id=serializer.validated_data["gift_id"],
+            recipient_player_ids=serializer.validated_data[
+                "recipient_player_ids"
+            ],
+        )
+        return Response(result)
