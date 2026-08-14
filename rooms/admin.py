80c268c564e3463e5ed4f15ef0726851e8bc48be
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-08-14 23:35:05 +0400

    Stage 15: show users and active gifts in room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,6 +7,7 @@ class RoomPlayerInline(admin.TabularInline):
     model = RoomPlayer
     extra = 0
     readonly_fields = ("joined_at",)
+    autocomplete_fields = ("user", "active_gift")
 
 
 @admin.register(GameRoom)
@@ -29,6 +30,23 @@ class GameRoomAdmin(admin.ModelAdmin):
 
 @admin.register(RoomPlayer)
 class RoomPlayerAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "room", "seat_index", "is_owner", "joined_at")
-    list_filter = ("is_owner",)
-    search_fields = ("name", "room__name", "room__owner_name")
+    list_display = (
+        "id",
+        "name",
+        "user",
+        "room",
+        "seat_index",
+        "is_owner",
+        "is_online",
+        "active_gift",
+        "joined_at",
+    )
+    list_filter = ("is_owner", "is_online", "room__restaurant")
+    search_fields = (
+        "name",
+        "user__username",
+        "user__email",
+        "room__name",
+        "room__owner_name",
+    )
+    autocomplete_fields = ("user", "room", "active_gift")
