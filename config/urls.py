9c9ef53dd344d87629e9e8aac1991bd9d6d77fa6
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-08-14 23:15:34 +0400

    Stage 14: serve uploaded gift images in development

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
@@ -16,3 +18,9 @@ urlpatterns = [
     path("api/", include("game.urls")),
     path("api/", include("gifts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
